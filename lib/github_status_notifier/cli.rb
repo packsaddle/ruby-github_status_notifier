fdfefc01b66a486b193161f95bfee5108f449501
sanemat <o.gata.ken@gmail.com>
2015-02-27 21:21:40 +0900

    chore(notify): add notify method

--- lib/github_status_notifier/cli.rb
+++ lib/github_status_notifier/cli.rb
@@ -12,5 +12,11 @@ module GithubStatusNotifier
     def version
       puts "GithubStatusNotifier version #{::GithubStatusNotifier::VERSION}"
     end
+
+    desc 'notify', 'Notify current status to GitHub status'
+
+    def notify
+      puts 'notify!!'
+    end
   end
 end
