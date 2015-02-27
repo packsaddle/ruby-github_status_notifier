fc61701051c0fd13fef274c46eac70f6140b449e
sanemat <o.gata.ken@gmail.com>
2015-02-27 21:16:49 +0900

    chore(gem): use thor

---
+++ lib/github_status_notifier/cli.rb
@@ -0,0 +1,16 @@
+require 'thor'
+
+module GithubStatusNotifier
+  class CLI < Thor
+    def self.exit_on_failure?
+      true
+    end
+
+    desc 'version', 'Show the GithubStatusNotifier version'
+    map %w(-v --version) => :version
+
+    def version
+      puts "GithubStatusNotifier version #{::GithubStatusNotifier::VERSION}"
+    end
+  end
+end
