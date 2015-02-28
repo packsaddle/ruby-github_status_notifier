0f6045582d813da940b19d27778f79d9679d3fa6
sanemat <o.gata.ken@gmail.com>
2015-02-28 19:12:10 +0900

    fix(notifier): fix ruby fail syntax

--- test/test_notifier.rb
+++ test/test_notifier.rb
@@ -1,9 +1,15 @@
 require_relative 'helper'
 module GithubStatusNotifier
   class TestNotifier < Test::Unit::TestCase
-    test 'version' do
-      assert do
-        !::GithubStatusNotifier::VERSION.nil?
+    def setup
+      @notifier = Notifier.new
+    end
+
+    sub_test_case '#decide_state' do
+      test 'not state nor exit-status' do
+        assert_raise(Error) do
+          @notifier.decide_state(nil, nil)
+        end
       end
     end
   end
