18cc20e2004fb93864856a565def6faa9f594db3
sanemat <o.gata.ken@gmail.com>
2015-02-28 17:24:36 +0900

    chore(logger): change log level

--- lib/github_status_notifier/client.rb
+++ lib/github_status_notifier/client.rb
@@ -2,7 +2,7 @@ module GithubStatusNotifier
   class Client < ::Saddler::Reporter::Github::Client
     def create_status(params)
       state = params.delete(:state)
-      logger.debug([slug, @repo.merging_sha, state, params])
+      logger.info(['create status with these:', slug, @repo.merging_sha, state, params])
       return_state = client.create_status(slug, @repo.merging_sha, state, params)
       logger.debug(return_state)
       return_state
