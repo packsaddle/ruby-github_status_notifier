47ab83f8961b68380f1c0d939d7eedc776870967
sanemat <o.gata.ken@gmail.com>
2015-02-28 15:42:59 +0900

    feat(notifier): add notifier logic

--- lib/github_status_notifier/cli.rb
+++ lib/github_status_notifier/cli.rb
@@ -58,9 +58,34 @@ module GithubStatusNotifier
       SUCCESS = 'success'
       ERROR = 'error'
       FAILURE = 'failure'
+      ALLOWED_STATUS = [PENDING, SUCCESS, ERROR, FAILURE]
       def notifier_notify(params)
-        if params[:state]
-          state = params[:state]
+        state = determine_state(params[:state], params[:exit_status])
+        pass_params = {
+          target_url: params[:target_url],
+          description: params[:description],
+          context: params[:context]
+        }
+        deliver_notification(pass_params.merge(state: state))
+      rescue StandardError => e
+        logger.error e.message
+        deliver_notification(pass_params.merge(state: ERROR))
+      end
+
+      def deliver_notification(params)
+      end
+
+      def determine_state(state, exit_status)
+        if state
+          return state.downcase if ALLOWED_STATUS.include?(state.downcase)
+          logger.error "state: #{state} is invalid. allowed #{ALLOWED_STATUS}"
+          fail ::GithubStatusNotifier::Error
+        elsif exit_status
+          return SUCCESS if exit_status.to_i == 0
+          return FAILURE
+        else
+          logger.error 'require state or exit_state'
+          fail ::GithubStatusNotifier::Error
         end
       end
     end
